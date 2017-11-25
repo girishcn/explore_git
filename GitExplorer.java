6cf4491be1b75401e9e1dc3f702b75b1bd547219
girishcn <33979409+girishcn@users.noreply.github.com>
2017-11-25 18:13:12 +0530

    Update GitExplorer.java

--- GitExplorer.java
+++ GitExplorer.java
@@ -4,5 +4,6 @@ public class ExploreGit
     public static void main(String[] args) throws Exception
     {
     	System.out.println("Hello Git");
+        System.out.println("Change in github");
     }
 }
